ac25d16e5c0631b6499861cf27d1d854b6feb33a
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-19 09:31:01 -0600

    add queries for save a hole list rounds find a round by user id and new round

--- queries.js
+++ queries.js
@@ -3,5 +3,27 @@ const knex = require('./connection');
 module.exports = {
   listHoles() {
     return knex('hole');
+  },
+  saveHole(hole) {
+    console.log(hole);
+    
+    return knex('hole')
+      .insert(hole)
+      .returning('*')
+      .then(record => record[0]);
+  },
+  listRounds() {
+    return knex('round');
+  },
+  roundsByUserId(userId) {
+    return knex('round')
+      .where('user_id', userId.userid)
+      .then(rounds => rounds);
+  },
+  newRound(userId) {
+    return knex('round')
+      .insert(userId)
+      .returning('*')
+      .then(record => record[0]);
   }
 };
